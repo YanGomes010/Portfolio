b63af9657882b7066df305a3b1089edf46da1a55
Yan Gomes <yangomesmd@gmail.com>
2022-12-28 20:31:51 -0300

    primeria modificação

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function ThemeMode(){
+    const textos = document.querySelector("#textos")
+
+            var element = document.body;
+            element.classList.toggle("dark-mode");
+            textos.classList.toggle("darkmode-color")
+            
+           
+}
