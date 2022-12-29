bb5710ecd94957221788592f60e3e87560ec57b2
Yan Gomes <yangomesmd@gmail.com>
2022-12-29 01:40:53 -0300

    teste

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
 
+
+window.addEventListener('resize', function () {
+    //var altura = window.innerHeight;
+    var largura = window.innerWidth;
+
+    if (largura < 768) {
+       let sec = document.querySelector('#sec-first')
+       sec.classList.remove("p-5")
+       let img = document.querySelector("#tt")
+       img.classList.remove("img-base-teclado")
+       img.classList.add("img-base-teclado-768")
+}});
+
+
+window.addEventListener('resize', function () {
+    //var altura = window.innerHeight;
+    var largura = window.innerWidth;
+
+    if (largura < 480) {
+       
+       let img = document.querySelector("#tt")
+       img.classList.remove("img-base-teclado-768")
+       img.classList.add("img-base-teclado-480")
+}});
+
+
+
+
+
+
+
+
+
+
+
+
 function ThemeMode(){
     const textos = document.querySelector("#textos")
-
             var element = document.body;
             element.classList.toggle("dark-mode");
             textos.classList.toggle("darkmode-color")
-            
-           
 }
+
+   function teste(){
+    const divCinza = document.querySelector(".offcanvas-backdrop")
+    divCinza.parentNode.removeChild(divCinza)
+    var f = document.getElementById("navbarOffcanvasLg");
+    f.classList.remove("show");
+    f.removeAttribute("aria-modal")
+    f.removeAttribute("role")
+   }
